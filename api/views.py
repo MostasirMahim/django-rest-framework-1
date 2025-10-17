7690e144f1a65777c42aa62d600f7d34fbe7cde5
Mostasir Mahim <mahimlive@gmail.com>
2025-10-18 05:20:20 +0600

    learn : serializers and use response & status.

--- api/views.py
+++ api/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
+from .serializers import StudentSerializer
 from students.models import Student
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
 
+##function based view
+
+@api_view(['GET', 'POST'])
 def studentsView(request):
     students = Student.objects.all()
-    lists = list(students.values())
-    return JsonResponse(lists, safe=False)
+    #lists = list(students.values()) #Manual Serialization
+    #return JsonResponse(lists, safe=False)
+
+    if(request.method == 'GET'):
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    if(request.method == 'POST'):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
