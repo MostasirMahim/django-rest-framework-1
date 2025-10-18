1df1428c6564739b8c9ac20fe3db7a704b8d951d
Mostasir Mahim <mahimlive@gmail.com>
2025-10-18 06:03:12 +0600

    learn : put and delete method.

--- api/views.py
+++ api/views.py
@@ -26,7 +26,7 @@ def studentsView(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def studentDetailsView(request, id):
     try:
         student = Student.objects.get(id=id)
@@ -36,4 +36,15 @@ def studentDetailsView(request, id):
     if request.method == 'GET':
         serialer = StudentSerializer(student)
         return Response(serialer.data, status=status.HTTP_200_OK)
-    return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else : 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
