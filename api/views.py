2e2d3611bb5494151ed86f59f261095c361f5034
Mostasir Mahim <mahimlive@gmail.com>
2025-10-21 19:49:45 +0600

    test : updated views

--- api/views.py
+++ api/views.py
@@ -5,7 +5,6 @@ from students.models import Student
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-# Create your views here.
 
 ##function based view
 
